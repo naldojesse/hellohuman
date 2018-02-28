26992370d44b4940b9c16b70e86de5a19e015bff
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-02-28 01:11:37 -0800

    finished exercise, controller takes params and checks for empty strings, views display full name

--- src/main/java/com/naldojesse/hellohuman/HellohumanApplication.java
+++ src/main/java/com/naldojesse/hellohuman/HellohumanApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @SpringBootApplication
@@ -14,9 +17,18 @@ public class HellohumanApplication {
 		SpringApplication.run(HellohumanApplication.class, args);
 	}
 
+//	@RequestMapping(value = "/test", method = RequestMethod.PO)
 	@RequestMapping("/")
-	public String index()  {
-		return "index.jsp";
+	public String index(@RequestParam(value="q", required = false) String f_name, @RequestParam(required=false) String l_name, ModelMap model)  {
+		if (f_name == null || l_name == null) {
+			return "index.jsp";
+		} else {
+
+			String full_name = f_name + l_name;
+			System.out.println(full_name);
+			model.addAttribute("name", full_name);
+			return "name.jsp";
+		}
 	}
 
 }
--- src/main/webapp/WEB-INF/name.jsp
+++ src/main/webapp/WEB-INF/name.jsp
@@ -2,7 +2,9 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <html>
-<h1>Hello Human!</h1>
+<a href="<c:url value="/"/>">Go Back</a>
+<h1>Hello <c:out value="${name}"/></h1>
 <p>Welcome to SpringBoot!</p>
+
 
 </html>
