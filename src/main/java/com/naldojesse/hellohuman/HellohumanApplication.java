34b60425bd445567063d9ce5f114c21e2a833293
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-02-28 00:32:53 -0800

    initial commit

---
+++ src/main/java/com/naldojesse/hellohuman/HellohumanApplication.java
@@ -0,0 +1,22 @@
+package com.naldojesse.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@SpringBootApplication
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index()  {
+		return "index.jsp";
+	}
+
+}
